4d243266c32d4f7d49e894bc6185da0b8f33cdda
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2020-11-13 04:12:48 +0100

    [front] top-bar + editor components added

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: EditorComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
