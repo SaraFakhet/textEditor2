4db76f652ced9867366a2d7a092a6d89b207b9d6
Sham Kazemi-Joestani <sham.kazemi-joestani@epita.fr>
2020-12-16 11:28:48 +0100

    Uncomment request in textBox

--- app.py
+++ app.py
@@ -106,11 +106,10 @@ def textBox(file):
     for f in list_open_files:
         if (f.filename == file):
             f.text = data['body']
-            """
+
             cur.execute("UPDATE files SET text = '" + data['body'] + "' WHERE filename ISLIKE '" + file + "'")
-            #cur.execute("INSERT INTO version VALUES ('"+ f.filename +"','" + f.text + "', NOW(),'" + data.user + "')") # FIXME a tester
+            cur.execute("INSERT INTO version VALUES ('"+ f.filename +"','" + f.text + "', NOW(),'" + data.user + "')") # FIXME a tester
             con.commit()
-            """
             break
 
     return jsonify(data)
