e3250aaa4ad8b6dedb322918587988764fc53f7e
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2020-12-16 12:17:59 +0100

    select a file to start

--- front/src/app/top-bar/top-bar.component.ts
+++ front/src/app/top-bar/top-bar.component.ts
@@ -5,6 +5,18 @@ import { VirtualTimeScheduler } from 'rxjs';
 import { DataService } from "../data.service";
 import { HttpClient } from '@angular/common/http';
 
+export interface AllFiles {
+  data: Array<[{
+    alignement: string,
+    bold: boolean,
+    filename: string,
+    font: string,
+    italic: boolean,
+    text: {body: string},
+    underline: boolean
+  }]>
+}
+
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
@@ -16,6 +28,7 @@ export class TopBarComponent implements OnInit {
   pseudo: string;
   fileName: string;
   channel: Channel;
+  allFiles: AllFiles;
 
   constructor(private data: DataService, private http: HttpClient) { }
 
@@ -32,10 +45,19 @@ export class TopBarComponent implements OnInit {
   }
 
   onSubmitFilename(form: NgForm) {
+    console.log("file name : " + form.value)
     this.data.changeFilename(form.value);
     this.data.changeChannel(form.value);
     this.closeFilename();
     this.http.get('http://localhost:5000/open-files/' + form.value).subscribe(data => {});
+  }
+
+  clickFilename(filename: string) {
+    console.log("file name : " + filename)
+    this.data.changeFilename(filename);
+    this.data.changeChannel(filename);
+    this.closeFilename();
+    this.http.get('http://localhost:5000/open-files/' + filename).subscribe(data => {});
   }
 
   openSaveFiles() { 
@@ -74,7 +96,11 @@ export class TopBarComponent implements OnInit {
   }
 
   openFilename() {
-    (document.querySelector('.bg-modal-filename') as HTMLInputElement).style.display = "flex";
+    let response = this.http.get<AllFiles>('http://localhost:5000/list-open-files').toPromise().then(data => {
+      this.allFiles = data;
+      console.log(JSON.stringify(this.allFiles));
+      (document.querySelector('.bg-modal-filename') as HTMLInputElement).style.display = "flex";
+    });
   }
 
   closeFilename() {
